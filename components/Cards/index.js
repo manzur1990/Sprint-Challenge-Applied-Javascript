437cdd79a634c2d01c5d3c6a35c4b3767d98f745
JM <manzur-1990@hotmail.com>
2020-04-10 12:59:45 -0500

    Could not finish on time: need to troubleshoot Cards and complete Questions

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,60 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+const cardData = document.querySelector("cards-container");
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then((response) => {
+    //Reference: {"topics":["javascript","bootstrap","technology","jquery","node.js"]}
+    const javascript = Object.entries(data.article.javascript);
+    const bootstrap = Object.entries(data.article.bootstrap);
+    const technology = Object.entries(data.article.technology);
+    const jquery = Object.entries(data.article.jquery);
+    const nodeJS = Object.entries(data.article.node);
+
+    const topics = [javascript, bootstrap, technology, jquery, nodeJS];
+
+    topics.forEach((article) => {
+      article.forEach((newArticle) => {
+        cardData.append(Cards(newArticle));
+      });
+    });
+  })
+
+  .catch((error) => {
+    console.log(`Error: `, error);
+  });
+
+
+
+function Cards(object) {
+  // create elements
+  const card = document.createElement("div");
+  const headline = document.createElement("div");
+  const author = document.createElement("div");
+  const imgCont = document.createElement("div");
+  const imgAuth = document.createElement("img");
+  const nameAuth = document.createElement("span");
+
+  // add class to elements
+  card.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imgCont.classList.add("img-container");
+  imgAuth.classList.add("img");
+
+  // create HTML structure
+card.appendChild('headline')
+card.appendChild('author')
+author.appendChild('imgCont')
+imgCont.appendChild('imgAuth')
+authror.appendChild('imgAuth')
+}
+
+// add content to elements
+
+
+
+// add to DOM
